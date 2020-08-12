536841fa2ddae35436ff4a4e41b787eb953332c0
ihlee <leeinhwan7@dgist.ac.kr>
2020-08-12 10:15:49 +0900

    merge_request_new

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.webNavigation.onCompleted.addListener(
   function (details) {
-    if (/merge_requests.*edit/.test(details.url)) {
+    if (/merge_requests.*(edit|new)/.test(details.url)) {
       chrome.tabs.executeScript({
         file: 'contents.js',
       });
