00b1c91398d88cc14bbe91bd4b38d51a2aa05a99
ihlee <leeinhwan7@dgist.ac.kr>
2021-02-19 11:07:17 +0900

    조건 변경

--- contents.js
+++ contents.js
@@ -7,23 +7,24 @@ function getFormatDate(date) {
   return year + "." + month + "." + day;
 }
 const name = localStorage.getItem("crsdevName");
+const reviewer = localStorage.getItem("reviewer");
 let text = "";
 text += "### Overview\n";
 text += "| 항목         | 내용         |\n";
 text += "|--------------|:-------------|\n";
 text += "| Review Order | 1 |\n";
-text += `| Review Date  | ${getFormatDate(new Date())} |\n`;
-text += `| Reviewer     | ${name} |\n`;
+text += `| Review Date  | ${getFormatDate(new Date())} |\n`; // 머지 올린 날짜
+text += `| Reviewer     | ${reviewer}, ${name} |\n`; // 내 이름, 리뷰어
 text += "\n";
 text += "### Comments\n";
 text += "| No           | Reviewer     | Result       | Comment      |\n";
 text += "|--------------|:-------------|:------------:|:-------------|\n";
-text += `| 1 | ${name} | Pass | 소스 확인 |\n`;
+text += `| 1 |  |  |  |\n`;
 text += "\n";
 text += "### Result (PASS/FAIL/FOLLOWUP)\n";
 text += "| Result       | Comment      |\n";
 text += "|--------------|:-------------|\n";
-text += "| Pass   | |\n";
+text += "|    | |\n";
 
 const target = document.getElementById("merge_request_description");
 target.value = text;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.getElementById("button").addEventListener("click", (e) => {
   const value = document.getElementById("crsdevName").value;
+  const reviewer = document.getElementById("reviewer").value;
   chrome.tabs.executeScript({
-    code: `localStorage.setItem('crsdevName', '${value}')`,
+    code: `localStorage.setItem('crsdevName', '${value}');` + `localStorage.setItem('reviewer', '${reviewer}');`,
   });
 });
